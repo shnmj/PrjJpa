93b32b343cf80ccad5ce4f7762dcd12269be0cbf
shnmj <shnmje@gmail.com>
2024-04-17 12:53:08 +0900

    Mustache 사용

---
+++ src/main/java/com/green/controller/HomeController.java
@@ -0,0 +1,36 @@
+package com.green.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	
+	@GetMapping("/hi")
+	public String hi() {
+		return "greetings";   // greentings.msutache
+		// 화면 출력할 template
+		// resources/template package에 생성
+	}
+	
+	@GetMapping("/hi2")
+	public String hi2(Model model) {
+		model.addAttribute("username", "SHIN");
+		return "greetings2"; // greetings2.mustache
+	}
+	
+	@GetMapping("/hi3")
+	public String hi3(Model model) {
+		model.addAttribute("username", "KIM");
+		return "greetings3"; // greetings3.mustache
+	}
+	
+	@GetMapping("/hi4")
+	public String hi4(Model model) {
+		model.addAttribute("username", "LEE");
+		return "greetings4"; // greetings4.mustache
+	}
+	
+}
